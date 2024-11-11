e2e09a3b06adf9c156fc9b020e8dba4a0067c2a9
mkabilan24 <127401361+mkabilan24@users.noreply.github.com>
2024-11-11 15:00:11 +0800

    removal of reduntant code

--- lib/Lossless/4_Lossless_IntrinsicImpedance.dart
+++ lib/Lossless/4_Lossless_IntrinsicImpedance.dart
@@ -22,24 +22,6 @@ void calc_lossless_intrinsicimpedance() {
   return;
 }
 
-void calc_E1() {
-  double intrinsicimpedanceValue = convertMathExpressionToDouble(lossless_intrinsicimpedance);
-  double magneticabsoluteH0Value = convertMathExpressionToDouble(magneticabsoluteH1);
-  double electricabsoluteE0Value =
-      intrinsicimpedanceValue * magneticabsoluteH0Value;
-  updateDouble(electricabsoluteE1, electricabsoluteE0Value);
-  print("|E_0| = $electricabsoluteE0Value.");
-}
-
-void calc_H1() {
-  double intrinsicimpedanceValue = convertMathExpressionToDouble(lossless_intrinsicimpedance);
-  double electricabsoluteE0Value = convertMathExpressionToDouble(electricabsoluteE1);
-  double magneticabsoluteH0Value =
-      electricabsoluteE0Value / intrinsicimpedanceValue;
-  updateDouble(electricabsoluteE1, electricabsoluteE0Value);
-  print("|H_0| = $magneticabsoluteH0Value.");
-}
-
 Widget Lossless_IntrinsicImpedanceDisplayWidget() {
   return Padding(
     padding: const EdgeInsets.all(5.0),
